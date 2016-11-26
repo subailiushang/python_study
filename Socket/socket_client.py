78e6a3e710fe57d279aff436726e6b560253dbc4
subailiushang <echozhangzihao@icloud.com>
2016-11-26 16:43:15 +0800

    client

---
+++ Socket/socket_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import socket
+
+HOST = '127.0.0.1'
+PORT = 3214
+
+s = socket.socket()
+
+try:
+	s.connect((HOST,PORT))
+	data = "hello"
+	while data:
+		s.sendall(data.endcode('utf-8'))
+		data = s.recv(1024)
+		print "Receive from Server:\n",data.decode('utf-8')
+		data = raw_input("Please input an info:\n")
+except socket.error as err:
+	print err
+finally:
+	s.close()
